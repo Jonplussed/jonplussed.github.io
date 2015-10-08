36efa013d66b187a7f46af97e97de075c002143d
Jon Childress <jon@childr.es>
2015-10-08 11:40:36 -0500

    pseudo-sections working correctly

--- src/Resume/Template.hs
+++ src/Resume/Template.hs
@@ -24,27 +24,13 @@ webpage :: Tag.Markup
 webpage = do
   Tag.docType
   Tag.html ! Attr.lang "en-US" $ do
-    Tag.head $ do
-      Tag.meta ! Attr.charset "UTF-8"
-      Tag.title $ Tag.text pageTitle
-      Tmpl.stylesheet "all" "stylesheets/normalize.css"
-      Tmpl.stylesheet "screen" "stylesheets/screen.css"
-      Tmpl.stylesheet "print" "stylesheets/print.css"
-    Tag.body $
-      Tag.div ! Attr.class_ "wrapper-outer" $
-        Tag.div ! Attr.class_ "wrapper-inner" $ do
-          Tag.header $
-            Tag.div ! Attr.class_ "header-content" $ do
-              Tag.h1 $ Tag.text pageTitle
-              navigation
-          Tag.main $ do
-            contents
-            navigation
+    pageHead
+    pageBody
 
 -- private functions
 
 contents :: Tag.Markup
-contents = mapM_ Tag.toMarkup Sect.sections
+contents = Tag.main $ mapM_ Tag.toMarkup Sect.sections
 
 navigation :: Tag.Markup
 navigation = Tag.nav . Tmpl.listItems $ map navLink Sect.sections
@@ -53,3 +39,41 @@ navLink :: Sect.Section -> Tag.Markup
 navLink sect =
   Tag.a ! Attr.href (Tag.textValue $ "#" <> Sect.ident sect) $
     Tag.text $ Sect.name sect
+
+pageHead :: Tag.Markup
+pageHead =
+  Tag.head $ do
+    Tag.meta ! Attr.charset "UTF-8"
+    Tag.title $ Tag.text pageTitle
+    Tmpl.stylesheet "all" "stylesheets/normalize.css"
+    Tmpl.stylesheet "screen" "stylesheets/screen.css"
+    Tmpl.stylesheet "print" "stylesheets/print.css"
+
+pageBody :: Tag.Markup
+pageBody =
+  Tag.body $
+    Tag.div ! Attr.class_ "wrapper-outer" $
+      Tag.div ! Attr.class_ "wrapper-inner" $ do
+        header
+        contents
+        footer
+
+header :: Tag.Markup
+header =
+  Tag.header ! Attr.class_ "pseudo-section" $
+    Tag.div ! Attr.class_ "pseudo-section-content" $ do
+      Tag.h1 $ Tag.text pageTitle
+      navigation
+
+footer :: Tag.Markup
+footer =
+  Tag.footer ! Attr.class_ "pseudo-section" $
+    Tag.div ! Attr.class_ "pseudo-section-content" $ do
+      navigation
+      copyright
+
+copyright :: Tag.Markup
+copyright =
+  Tag.p $
+    Tag.small $
+      Tag.preEscapedText "&copy; 2015 Jonathan Childress"
